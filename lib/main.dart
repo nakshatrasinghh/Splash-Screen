858d9d243a54e39b678565e8a13dd39dd8d9126b
nakshatrasinghh <nakshatradsu@gmail.com>
2021-09-14 10:34:53 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:splash_screen/pages/splash.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Splash Screens',
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ lib/pages/splash.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            decoration: const BoxDecoration(color: Colors.greenAccent),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 50.0,
+                      child: Icon(
+                        Icons.local_grocery_store,
+                        color: Colors.black,
+                        size: 50.0,
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 10.0)),
+                    Text(
+                      "NakstarKart",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24.0,
+                          color: Colors.black),
+                    )
+                  ],
+                ),
+              ),
+              // Expanded(
+              //   flex: 1,
+              // child: Column(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: [
+              //     CircularProgressIndicator.adaptive(value: 3.0,),
+              //     Padding(padding: const EdgeInsets.only(top: 20.0))
+              //   ],
+              // ),)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
